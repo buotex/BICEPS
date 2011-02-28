62a91b4e6078c51b13cb3de18b68a7acf75065e4
Buote Xu <Buote.Xu@gmail.com>
2011-02-28 02:55:26 +0100

    added documentation mainpage

---
+++ doc/Mainpage.h
@@ -0,0 +1,40 @@
+/*!
+
+\mainpage A Novel Error Tolerant Search Strategy for Cross-Species Proteomics 
+
+
+\section sec_intro Introduction
+
+\c TODO 
+
+\section sec_license Licensing
+\c TODO 
+
+\section sec_download Download
+The source repository,including packaged windows binaries is available from 
+http://hci.iwr.uni-heidelberg.de/MIP/Software/
+
+\section sec_install Installation
+Using the binary, just place it in a convenient location, take care that the \c Models
+folder is in the same place.
+
+
+\subsection sec_install_src Building from Source
+The following illustrates how to build the examples, tests and
+distribution packages. Bundling \c libfbi requires a working CMake build system
+(available from http://cmake.org/) and CMake >= 2.6.
+
+With cmake in the system path, the build process is
+\verbatim
+ tar xvzf biceps-xxxxxxx.tar.gz
+ mkdir biceps
+ cd biceps
+ ccmake ../biceps
+ make
+\endverbatim
+And, optionally:
+\verbatim
+ make package
+\endverbatim
+*/
+
