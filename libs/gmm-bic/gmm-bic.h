adc31264411775b05e998066fc62f40724f9e97b
buote <buote@vm.(none)>
2011-06-15 20:49:20 +0200

    cleaning up pages-branch

--- libs/gmm-bic/gmm-bic.h
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <cmath>
-#include <vector>
-void gmm_bic(int numSegments, int numScores, const char* filename, 
-             std::vector<double> & bicMu, std::vector<double> & bicSigma, std::vector<int> & labels);
-
